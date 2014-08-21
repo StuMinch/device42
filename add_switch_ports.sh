6ec533c194dd5474ccb3fc8546b6cffdafe0a1c7
stuartminch <stuarttm@me.com>
2014-08-20 17:15:54 -0700

    Update add_switch_ports.sh

--- add_switch_ports.sh
+++ add_switch_ports.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-for i in {6..48}
+# Author: Stuart Minchington (stuarttm@me.com)
+# Description: A script that utilizes device42's REST API to add multiple switch ports to a device
+# For obvious reasons I am using faux details for the username, password, and IP address.
+for i in {1..48}
 do
-   curl -X POST -d "port=SW1-P5&switch=sj1-sv5-0103-cmls-01&up_admin=no"  -u 'username:password' https://192.168.0.10/api/1.0/switchports/ --insecure
+   curl -X POST -d "port=SW1-P$i&switch=sj1-sv5-0103-cmls-01&up_admin=no"  -u 'username:password' https://192.168.0.10/api/1.0/switchports/ --insecure
 done
