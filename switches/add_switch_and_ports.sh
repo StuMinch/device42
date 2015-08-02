71dc5dd71d85feae5b547d36d870ee294d5555ed
Stuart Minchington <stuartminch@users.noreply.github.com>
2015-08-01 21:29:48 -0700

    Update add_switch_and_ports.sh

--- switches/add_switch_and_ports.sh
+++ switches/add_switch_and_ports.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Add the switch hardware
-curl -X POST -d "name=sv5-rack2-switchB&type=physical&hardware=30&is_it_switch=yes&in_service=yes&service_level=production&macaddress=08:9e:01:ce:c0:43" -u 'username:password' https://10.100.0.114/api/device/ --insecure
+# Enter attributes here:
+SWITCH_NAME=switchA
+HARDWARE=AS6701-32X
+MAC_ADDRESS=70:72:cf:cb:f6:8a
+IP_ADDRESS=10.100.254.21
 
-sleep 5
+# Add the switch hardware
+curl -X POST -d "name=$SWITCH_NAME&type=physical&hardware=$HARDWARE&is_it_switch=yes&in_service=yes&service_level=production&macaddress=$MAC_ADDRESS" -u 'username:password' https://192.168.0.1/api/device/ --insecure
+
+sleep 2
+
+# Add the MGMT IP address
+curl -X POST -d "device=$SWITCH_NAME&ipaddress=$IP_ADDRESS" -u 'username:password' https://192.168.0.1/api/ip/ --insecure
+
+sleep 2
 
 # Add the switch ports
-for i in {1..52}
+for i in {1..32}
 do
-   curl -X POST -d "port=SW2-P$i&switch=sv5-rack2-switchB"  -u 'username:password' https://10.100.0.114/api/1.0/switchports/ --insecure
+   curl -X POST -d "port=swp$i-s0&$SWITCH_NAME"  -u 'username:password' https://192.168.0.1/api/1.0/switchports/ --insecure
+   curl -X POST -d "port=swp$i-s1&$SWITCH_NAME"  -u 'username:password' https://192.168.0.1/api/1.0/switchports/ --insecure
+   curl -X POST -d "port=swp$i-s2&$SWITCH_NAME"  -u 'username:password' https://192.168.0.1/api/1.0/switchports/ --insecure
+   curl -X POST -d "port=swp$i-s3&$SWITCH_NAME"  -u 'username:password' https://192.168.0.1/api/1.0/switchports/ --insecure
+   sleep 2
 done
